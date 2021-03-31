dfc54d05908183d8e6db6f7ed9b9bc0ec8da182e
Yudu Ban <banyudu@gmail.com>
2021-03-31 11:09:35 +0800

    wip: implement fetch data logic

--- src/index.js
+++ src/index.js
@@ -6,11 +6,17 @@
 const YAML = require('yaml')
 const fs = require('fs').promises
 const path = require('path')
+const { walk, fetchReposInfo } = require('./utils')
 
 const buildReadme = async () => {
   const dataFile = await fs.readFile(path.join(__dirname, 'data.yml'), 'utf-8')
   const data = YAML.parse(dataFile)
-  console.log(data)
+  const repoSet = new Set()
+  walk(data, node => node.repos.forEach(repo => repoSet.add(repo)))
+  const repos = Array.from(repoSet)
+  console.log('repos: ', repos)
+  const repoInfo = await fetchReposInfo(repos)
+  console.log('info: ', repoInfo)
 }
 
 buildReadme()
--- src/utils.js
+++ src/utils.js
@@ -3,11 +3,47 @@
  * @description utils 
  */
 
+const { graphql } = require('@octokit/graphql')
+const base62 = require('base62/lib/ascii')
+
+const headers = {
+  authorization: `Bearer ${process.env.GITHUB_TOKEN}`
+}
+
 /**
  * Fetch info for repos
  * @param {string[]} repos 
  * @returns {object} { [repoName]: { description: '', star: '', collaborators: [] } }
  */
 exports.fetchReposInfo = async (repos) => {
+  const queries = repos.map(repo => {
+    const [owner, name] = repo.split('/')
+    const key = 'A' + base62.encode(repo)
+    return `
+      ${key}: repository (owner: "${owner}", name: "${name}") {
+        name,
+        description
+      }`
+  })
+  const query = `{${queries.join(',')}}`
+  const queryRes = await graphql(query, { headers })
+  console.log(queryRes)
+}
 
+const _walk = (tree, cb, pathes = []) => {
+  if (!tree) {
+    return
+  }
+  const keys = Object.keys(tree)
+  keys.forEach(key => {
+    const fullpath = [...pathes, key]
+    const fullpathStr = fullpath.join('.')
+    const node = tree[key]
+    cb(node, fullpathStr)
+    _walk(node.children, cb, fullpath)
+  })
+}
+
+exports.walk = (tree, cb) => {
+  _walk(tree, cb)
 }
